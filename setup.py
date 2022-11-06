ff0a5ea639f9aa3b195dc91e1c97b83614599be8
Zsolt Pandi <zpa.git@gmail.com>
2022-11-06 17:36:02 +0100

    add package version number and fix wrong package dependency

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='tempcontrol',
+    version='1.0',
     packages=['tempcontrol'],
     include_package_data=True,
     install_requires=[
-        'flask', 'matplotlib', 'paho-mqtt', 'gammu',
+        'flask', 'matplotlib', 'paho-mqtt', 'python-gammu',
     ],
 )
